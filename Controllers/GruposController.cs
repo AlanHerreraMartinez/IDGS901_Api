a54fa13c73bcf569e4ec417650127cc0c65a3b0f
AlanHerreraMartinez <alanherreramartinez441@gmail.com>
2023-07-08 19:10:57 -0600

    CLASE 08/07/23 AHM

---
+++ Controllers/GruposController.cs
@@ -0,0 +1,102 @@
+﻿using IDGS901_Api.context;
+using IDGS901_Api.models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDGS901_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GruposController : ControllerBase
+    {
+        public readonly AppDbContext _Context;
+
+        public GruposController(AppDbContext context)
+        {
+            _Context = context;
+        }
+        [HttpGet]//api/<Grupos>
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_Context.Alumnos.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("{id}",Name ="Alumnos")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var alum=_Context.Alumnos.FirstOrDefault(x => x.Id == id);
+                return Ok(alum);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public ActionResult<Alumnos> Post([FromBody]Alumnos alum)
+        {
+            try
+            {
+                _Context.Alumnos.Add(alum);
+                _Context.SaveChanges();
+                return CreatedAtRoute("Alumnos", new {id=alum.Id},alum);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Alumnos alum)
+        {
+            try
+            {
+                if (alum.Id == id)
+                {
+                    _Context.Entry(alum).State =EntityState.Modified;
+                    _Context.SaveChanges();
+                    return CreatedAtRoute("Alumnos", new {id=alum.Id},alum);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var alum = _Context.Alumnos.FirstOrDefault(x => x.Id == id);
+                if (alum != null)
+                {
+                    _Context.Remove(alum);
+                    _Context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using IDGS901_Api.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDGS901_Api.context
+{
+    public class AppDbContext:DbContext
+    {
+        private const string conectionstring = "conexion";
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+            base(options){ }
+        public DbSet<Alumnos> Alumnos { get; set; }
+    }
+}
